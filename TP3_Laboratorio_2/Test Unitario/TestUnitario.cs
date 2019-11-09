78f1757cbcc4b29785cdd8122e9df813d157ff2f
kevinahumada7 <kevinahumada7@gmail.com>
2019-11-09 00:52:18 -0300

    tp3 terminado lpm

--- TP3_Laboratorio_2/Test Unitario/TestUnitario.cs
+++ TP3_Laboratorio_2/Test Unitario/TestUnitario.cs
@@ -15,13 +15,7 @@ namespace Test_Unitario
         {
             try
             {
-                Universidad utn = new Universidad();
-                Universidad uba = new Universidad();
-
-                utn = uba;
-                uba = null;
-                Universidad.Guardar(utn);
-                Universidad.Guardar(uba);
+                Universidad utn = Universidad.Leer();
             }
             catch (Exception e)
             {
@@ -30,15 +24,15 @@ namespace Test_Unitario
         }
 
         [TestMethod]
-        public void DniInvalidoExceptionTest()
+        public void NacionalidadExceptionTest()
         {
             try
             {
-                Alumno alumno = new Alumno(1, "Ezequiel", "Onysz", "43.234.520", Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+                Alumno alumno = new Alumno(1, "Ezequiel", "Onysz", "43234520", Persona.ENacionalidad.Extranjero, Universidad.EClases.Legislacion);
             }
             catch (Exception e)
             {
-                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+                Assert.IsInstanceOfType(e, typeof(NacionalidadInvalidaException));
             }
         }
 
@@ -47,14 +41,15 @@ namespace Test_Unitario
         {
             Alumno alumno = new Alumno(1, "Alvaro", "Elena", "97255120", Persona.ENacionalidad.Extranjero, Universidad.EClases.SPD);
             alumno.StringToDNI = "98468742";
-
             Assert.AreEqual(98468742, alumno.DNI);
+            alumno.DNI = 97255120;
+            Assert.AreEqual(97255120, alumno.DNI);
         }
 
         [TestMethod]
         public void ValoresNulos()
         {
-            Profesor profe = new Profesor(1, "Brian Ignacio", "Ahumada", "16516", Persona.ENacionalidad.Argentino);
+            Profesor profe = new Profesor(1, "Brian Ignacio", "Ahumada", "37516320", Persona.ENacionalidad.Argentino);
             Assert.IsNotNull(profe.Apellido);
             Assert.IsNull(profe.Nombre);
         }
